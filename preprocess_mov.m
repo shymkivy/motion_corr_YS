955e3e1b6b59c83eb1d292236a4ce79ef62090ad
yuriy <shymkivy@gmail.com>
2023-01-30 17:50:43 -0500

    updates

--- preprocess_mov.m
+++ preprocess_mov.m
@@ -24,7 +24,7 @@ addpath([pwd2 '\functions']);
 params.load_dir = 'D:\data\AC\2p\2020\M8_5_21_20_missmatch';  % 
 params.save_dir = 'F:\AC_data\caiman_data_missmatch';
 
-params.load_fname = 'A2_ammn-001';  % can be a dir if is a list of tiffs
+params.load_fname = 'A2_ammn-001';  % can be a dir if is a Prairie list of tiffs
 params.save_fname = 'M8_im1_A2_ammn1_5_21_20';
 
 params.im_target_fname = ''; % cuts mat file with target for moco, string or cell of strings
